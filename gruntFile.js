f91f6562d3ec4b92faa6bafa6f6976a6e157a29e
luke <lukes611@gmail.com>
2016-09-19 16:18:07 +1000

    added sass support in grunt

--- gruntFile.js
+++ gruntFile.js
@@ -3,11 +3,17 @@ module.exports = function(grunt){
     grunt.initConfig({
         jshint:{
             src : 'client/js/index.js'
+        },
+        sass:{
+            files : {
+                'scss/index.sass' : 'client/css/index.css'
+            }
         }
     });
     
     //load plugins:
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'sass']);
 };
