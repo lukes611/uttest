5a744d0755a051b4a7c4ddddd1d6a27424ff764a
luke <lukes611@gmail.com>
2016-09-19 16:05:26 +1000

    finished js validation code

--- client/js/index.js
+++ client/js/index.js
@@ -3,15 +3,24 @@
 var IndexHTMLCode = {
     radioId : 'otherCelebrationType',
     inputTagId : 'otherDescription',
+    errorTagId : 'userWarning',
     valid : function(){
-        var otherTag = document.getElementById(this.radioId);
-        var inputTag = document.getElementById(this.inputTagId);
-        if(otherTag == null || inputTag == null) throw new Error('unable to access dom elements in IndexHTMLCode.valid() function');
+        var otherTag    = document.getElementById(this.radioId);
+        var inputTag    = document.getElementById(this.inputTagId);
+        var warningTag  = document.getElementById(this.errorTagId);
+        if(otherTag == null || inputTag == null || warningTag == null) throw new Error('unable to access dom elements in IndexHTMLCode.valid() function');
         
         var otherChecked = otherTag.checked;
-        var inputTagHasData = inputTag.innerHTML.length > 0;
+        var inputTagHasData = inputTag.value.length > 0;
         
-        return !otherChecked || inputTagHasData; //either other is not checked or the user has put data into the input
+        var isValid = !otherChecked || inputTagHasData;
+        
+        if(!isValid){
+            alert('warning: you have checked "other" in "Celebration type" but have not included a description');
+            warningTag.style.display = 'block';
+        }else warningTag.style.display = 'none';
+        
+        return isValid; //either other is not checked or the user has put data into the input
     }
 };
 
