ab8cba30b9d9dd5ed062884b058b3a4158bb9106
luke <lukes611@gmail.com>
2016-09-19 15:51:43 +1000

    added validation function for form

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,17 @@
 
+/* an object for processing the form located in index.html   */
+var IndexHTMLCode = {
+    radioId : 'otherCelebrationType',
+    inputTagId : 'otherDescription',
+    valid : function(){
+        var otherTag = document.getElementById(this.radioId);
+        var inputTag = document.getElementById(this.inputTagId);
+        if(otherTag == null || inputTag == null) throw new Error('unable to access dom elements in IndexHTMLCode.valid() function');
+        
+        var otherChecked = otherTag.checked;
+        var inputTagHasData = inputTag.innerHTML.length > 0;
+        
+        return !otherChecked || inputTagHasData; //either other is not checked or the user has put data into the input
+    }
+};
 
-alert('hi there')
